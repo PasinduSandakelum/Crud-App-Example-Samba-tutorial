e8c91fbcdf9656fa6909251c2fb9f93a1029e08a
PasinduSandakelum <tmpsthilakarathna2017@gmail.com>
2018-10-02 16:19:38 +0530

    Add forntend and backend

---
+++ CrudBackend/src/main/java/com/sgic/leavemanagement/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.sgic.leavemanagement.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sgic.leavemanagement.entity.User;
+import com.sgic.leavemanagement.repository.UserRepository;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins="http://localhost:4200", allowedHeaders="*")
+public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/users")
+	public List<User> getUsers() {
+		return userRepository.findAll();
+	}
+	
+	@GetMapping("/user/{id}")
+	public User getUser(@PathVariable("id") Long id) {
+		User user = userRepository.getOne(id);
+		return user;
+	}
+	
+	@DeleteMapping("/user/{id}")
+	public boolean deleteUser(@PathVariable("id") Long id) {
+		userRepository.deleteById(id);
+		return true;
+	}
+	
+	@PostMapping("/user")
+	public User createUser(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+	
+	@PutMapping("/user")
+	public User updateUser(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+	
+}
---
+++ CrudBackend/src/main/java/com/sgic/leavemanagement/entity/User.java
@@ -0,0 +1,50 @@
+package com.sgic.leavemanagement.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class User implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String fname;
+	private String lname;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+}
